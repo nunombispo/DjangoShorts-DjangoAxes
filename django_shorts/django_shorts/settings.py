687daca584e44b9b0c72829b2221d5b2abe4a73e
Nuno Bispo <nuno.m.bispo@gmail.com>
2021-09-02 10:38:31 +0200

    Updated home page and settings.py

--- django_shorts/django_shorts/settings.py
+++ django_shorts/django_shorts/settings.py
@@ -37,7 +37,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_axes'
+    'django_axes',
+
+    # Axes app can be in any position in the INSTALLED_APPS list.
+    'axes',
+]
+
+AUTHENTICATION_BACKENDS = [
+    # AxesBackend should be the first backend in the AUTHENTICATION_BACKENDS list.
+    'axes.backends.AxesBackend',
+
+    # Django ModelBackend is the default authentication backend.
+    'django.contrib.auth.backends.ModelBackend',
 ]
 
 MIDDLEWARE = [
@@ -48,7 +59,17 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    # AxesMiddleware should be the last middleware in the MIDDLEWARE list.
+    # It only formats user lockout messages and renders Axes lockout responses
+    # on failed user authentication attempts from login views.
+    # If you do not want Axes to override the authentication response
+    # you can skip installing the middleware and use your own views.
+    'axes.middleware.AxesMiddleware',
 ]
+
+# Block only on username
+AXES_ONLY_USER_FAILURES=True
 
 ROOT_URLCONF = 'django_shorts.urls'
 
